dc308a123a8ef6bee3b83f97f52009260bd41a62
Lisa Schwetlick <mail@lisaschwetlick.de>
2020-04-08 15:49:33 +0200

    made tools pip installable

---
+++ setup.py
@@ -0,0 +1,12 @@
+""" Set up RM Converting"""
+from setuptools import setup, find_packages
+
+setup(
+    name='rm_tools',
+    version='1.0',
+    description='reMarkable file format conversion',
+    author='Lisa Schwetlick',
+    author_email='lisa.schwetlick@uni-potsdam.de',
+    packages=find_packages(),
+    install_requires=[]
+)
